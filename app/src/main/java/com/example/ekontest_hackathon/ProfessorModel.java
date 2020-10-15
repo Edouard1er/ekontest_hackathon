56f73b8d02d92b2bbb436fd5baa6ab6a37556bfd
Amos <amosdorceus2010@gmail.com>
2020-10-15 10:40:46 -0400

    completing professorModel

--- app/src/main/java/com/example/ekontest_hackathon/ProfessorModel.java
+++ app/src/main/java/com/example/ekontest_hackathon/ProfessorModel.java
@@ -10,6 +10,11 @@ import com.google.firebase.database.DatabaseReference;
 import java.util.List;
 
 public class ProfessorModel extends FreelancerModel implements Parcelable {
+    private String id;
+    private PersonalInformationModel personalInformationModel;
+    private AcademicInformationModel academicInformationModel;
+    private ProfilModel profilModel;
+    private AvisModel avisModel;
     private DatabaseReference databaseReference;
     private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
     public ProfessorModel() {
@@ -37,18 +42,76 @@ public class ProfessorModel extends FreelancerModel implements Parcelable {
     }
 
     @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public PersonalInformationModel getPersonalInformationModel() {
+        return personalInformationModel;
+    }
+
+    @Override
+    public void setPersonalInformationModel(PersonalInformationModel personalInformationModel) {
+        this.personalInformationModel = personalInformationModel;
+    }
+
+    @Override
+    public AcademicInformationModel getAcademicInformationModel() {
+        return academicInformationModel;
+    }
+
+    @Override
+    public void setAcademicInformationModel(AcademicInformationModel academicInformationModel) {
+        this.academicInformationModel = academicInformationModel;
+    }
+
+    @Override
+    public ProfilModel getProfilModel() {
+        return profilModel;
+    }
+
+    @Override
+    public void setProfilModel(ProfilModel profilModel) {
+        this.profilModel = profilModel;
+    }
+
+    @Override
+    public AvisModel getAvisModel() {
+        return avisModel;
+    }
+
+    @Override
+    public void setAvisModel(AvisModel avisModel) {
+        this.avisModel = avisModel;
+    }
+
+    @Override
     public int describeContents() {
         return 0;
     }
 
     @Override
     public void writeToParcel(Parcel dest, int flags) {
-
+        dest.writeString(this.id);
+        dest.writeParcelable(this.personalInformationModel,flags);
+        dest.writeParcelable(this.academicInformationModel,flags);
+        dest.writeParcelable(this.profilModel,flags);
+        dest.writeParcelable(this.avisModel,flags);
     }
 
     // example constructor that takes a Parcel and gives you an object populated with it's values
     private ProfessorModel(Parcel in) {
-
+        this.id= in.readString();
+        this.personalInformationModel = in.readParcelable(getClass().getClassLoader());
+        this.academicInformationModel = in.readParcelable(getClass().getClassLoader());
+        this.profilModel = in.readParcelable(getClass().getClassLoader());
+        this.avisModel = in.readParcelable(getClass().getClassLoader());
 
     }
 
