f7bfa1d389e682386c451c3c3dd9d81c72a71dc4
Amos <amosdorceus2010@gmail.com>
2020-10-16 12:27:25 -0400

    message

---
+++ app/src/main/java/com/example/ekontest_hackathon/AvisFreelancerActivity.java
@@ -0,0 +1,13 @@
+package com.example.ekontest_hackathon;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AvisFreelancerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_avis_freelancer);
+    }
+}
---
+++ app/src/main/java/com/example/ekontest_hackathon/InterfaceAvis.java
@@ -0,0 +1,4 @@
+package com.example.ekontest_hackathon;
+
+public interface InterfaceAvis {
+}
